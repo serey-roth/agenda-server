23fb52f94f8e3e3eb0cbab3911f6e6e19f9047fa
serey-roth <sereyratanakroth@gmail.com>
2022-12-14 15:47:45 -0800

    cors error

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ import projectsRoutes from './routes/projectsRoutes.js'
 import usersRoutes from './routes/usersRoutes.js'
 
 const app = express();
-app.use(cors());
+app.use(cors({
+        origin: '*',
+        optionsSuccessStatus: 200,
+}));
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -23,11 +26,6 @@ app.use('/users', usersRoutes);
 app.get('/', (req, res) => {
         res.send("Hello to Agenda!");
 })
-app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-});
 
 const PORT = process.env.PORT || 5000;
 
