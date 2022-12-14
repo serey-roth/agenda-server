2b4ff0fe53fafc131b0d3a054fbd843c8429607a
serey-roth <sereyratanakroth@gmail.com>
2022-12-14 15:40:27 -0800

    fixed cors error

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.use('/users', usersRoutes);
 app.get('/', (req, res) => {
         res.send("Hello to Agenda!");
 })
+app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+});
 
 const PORT = process.env.PORT || 5000;
 
